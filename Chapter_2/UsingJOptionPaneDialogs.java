6a09741fc950340ef6f000efc75129adbe0bdac9
JayoldJosiahGarcia-UCM <jayoldjosiahg@gmail.com>
2025-09-08 19:37:50 +0800

    Add files via upload

---
+++ Chapter_2/UsingJOptionPaneDialogs.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class UsingJOptionPaneDialogs{
+    public static void main(String[] args) {
+    
+    String userName = "";
+    int confirmation;
+
+        do { 
+        userName = JOptionPane.showInputDialog(null,"What's your name?");
+
+        
+        confirmation = JOptionPane.showConfirmDialog(null,"Do you want to confirm your name?",
+                                                          "Confirm",
+                                                          JOptionPane.YES_NO_OPTION);
+        } while (confirmation == 1);
+
+        JOptionPane.showMessageDialog(null,"Hi there "+userName+"!");
+
+    }
+}
