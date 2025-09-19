af2360123d8e9513b52597cc476687fa43c46a8e
JayoldJosiahGarcia-UCM <jayoldjosiahg@gmail.com>
2025-09-19 09:13:04 +0800

    moved DemoGrossPay.java & CompanyInfo to Chapter_3

--- ActivityCodes/DemoGrossPay.java
+++ ActivityCodes/DemoGrossPay.java
@@ -6,22 +6,7 @@ class DemoGrossPay {
         double hoursWorked1 = 10;
         double hoursWorked2 = 25;
         double hoursWorked3 = 37.5;
-        /*Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter hours worked: ");
-        double hoursWorked1 = scanner.nextDouble();
-        System.out.print("Enter hours worked: ");
-        double hoursWorked2 = scanner.nextDouble();
-        System.out.print("Enter hours worked: ");
-        double hoursWorked3 = scanner.nextDouble();
-       String DisplayString = hoursWorked1 + " hours at " + HOURLY_RATE + " per hour is " 
-                               + calculateGrossPay(hoursWorked1, HOURLY_RATE) + "\n"
-                             + hoursWorked2 + " hours at " + HOURLY_RATE + " per hour is " 
-                               + calculateGrossPay(hoursWorked2, HOURLY_RATE) + "\n"
-                             + hoursWorked3 + " hours at " + HOURLY_RATE + " per hour is " 
-                               + calculateGrossPay(hoursWorked3, HOURLY_RATE);
-        
-        System.out.println(DisplayString);
-        scanner.close();*/
+
         calculateGrossPay(hoursWorked1, hoursWorked2, hoursWorked3, HOURLY_RATE);
     }    
     public static void calculateGrossPay(double hoursWorked1, double hoursWorked2,double hoursWorked3, double HOURLY_RATE){
