ee59df94be045d5daccfb565e3f69ddec8646a7e
JayoldJosiahGarcia-UCM <jayoldjosiahg@gmail.com>
2025-09-19 12:24:30 +0800

    practice

--- Chapter_3/DemoGrossPay.java
+++ Chapter_3/DemoGrossPay.java
@@ -7,9 +7,9 @@ class DemoGrossPay {
         double hoursWorked2 = 25;
         double hoursWorked3 = 37.5;
 
-        calculateGrossPay(hoursWorked1, hoursWorked2, hoursWorked3, HOURLY_RATE);
+        calculateGross(hoursWorked1, hoursWorked2, hoursWorked3, HOURLY_RATE);
     }    
-    public static void calculateGrossPay(double hoursWorked1, double hoursWorked2,double hoursWorked3, double HOURLY_RATE){
+    public static void calculateGross(double hoursWorked1, double hoursWorked2,double hoursWorked3, double HOURLY_RATE){
         double grossPay1 = hoursWorked1 * HOURLY_RATE;
         double grossPay2 = hoursWorked2 * HOURLY_RATE;
         double grossPay3 = hoursWorked3 * HOURLY_RATE;
