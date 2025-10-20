6cce4c9bee77d94acaaf5dbcc0006ef5b76570af
JayoldJosiahGarcia-UCM <jayoldjosiahg@gmail.com>
2025-10-20 11:03:32 +0800

    finalized

--- Chapter_6/DisplayBankBalance.java
+++ Chapter_6/DisplayBankBalance.java
@@ -8,9 +8,8 @@ class DisplayBankBalance {
         double balance = scanner.nextDouble();
         
 
-            System.out.println("Do you want to see next year's balance?\n"
-                              + "Enter 1 for yes or any number for no >>"
-                              );
+            System.out.println("Do you want to see next year's balance?");
+            System.out.print("Enter 1 for yes or any number for no >>");
 
         int choice = scanner.nextInt();
         int year = 1;
@@ -19,10 +18,9 @@ class DisplayBankBalance {
             balance += balance * 0.03;
 
             System.out.printf("After year "+ year +" at 0.03 interest rate, balance is "+ balance + "\n");
-
-            System.out.println("Do you want to see the balance at the end of another year?\n"
-                              + "Enter 1 for yes or any number for no >>"
-                              );
+            System.out.println();
+            System.out.println("Do you want to see the balance at the end of another year?");
+            System.out.print("Enter 1 for yes or any number for no >>");
             choice = scanner.nextInt();
             if(choice != 1){
                 break;
