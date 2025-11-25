a93278091659bde9a147353b8f734c6157c2f579
JayoldJosiahGarcia-UCM <jayoldjosiahg@gmail.com>
2025-11-25 11:17:12 +0800

    uploaded CompareString activity

---
+++ Chapter_7/CompareString.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class CompareString {
+    public static void main(String[] args) {
+        String name = "Carmen";
+        String inputName;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name > ");
+        inputName = scanner.nextLine();
+
+        if(name.equals(inputName)){
+            System.out.println(inputName + " is equal to " + name);
+        }
+        else{
+            System.out.println(inputName + " is not equal to " + name);
+        }
+    }
+}
